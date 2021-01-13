e14658fa5faf31ae32081825946e27baa4e409cd
Nickolas Gupton <nickolas@gupton.xyz>
2021-01-13 09:55:27 -0500

    Clean up some duplicate code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,28 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-   'use strict';
-   var spinning = false;
+   let homeButton = document.querySelector('#homeButton');
+   let blogButton = document.querySelector('#blogButton');
+   let currentURL = window.location.toString();
    
-   document.querySelector('#monkey').addEventListener('click', function () {
+   // I can feel my sanity slipping into the regex abyss
+   let urlRegex = /^http[s]{0,1}:\/\/.+\/[\d]{4}\/[\d]{2}\/[\d]{2}\/.+$/;
+   
+   if (currentURL.includes('/blog') || currentURL.match(urlRegex) !== null) {
+      blogButton.classList.add('selected');
+   } else {
+      homeButton.classList.add('selected');
+   }
+   
+   let spinning = false;
+   let monkey = document.querySelector('#monkey');
+   
+   monkey.addEventListener('click', function () {
       if (!spinning) {
          spinning = true;
-         document.querySelector('#monkey').classList.add('monkeySpin');
+         monkey.classList.add('monkeySpin');
          
          setTimeout(function () {
-            document.querySelector('#monkey').classList.remove('monkeySpin');
+            monkey.classList.remove('monkeySpin');
             spinning = false;
          }, 1000);
       }
