91f32ab65943b173679ea17da93087e53ab21d30
Nickolas Gupton <nickolas@gupton.xyz>
2025-11-22 22:46:06 -0600

    Change CloudFlare channel

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -2,7 +2,7 @@
 
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --channel Current --install-dir ./dotnet
+./dotnet-install.sh --channel 10.0 --install-dir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools
 
